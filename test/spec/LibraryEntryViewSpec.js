aef114fdbb687771b1c6943f0089de6156e32347
Phillip Alexander <git@phillipalexander.io>
2014-03-12 09:57:51 -0700

    Initial commit

---
+++ test/spec/LibraryEntryViewSpec.js
@@ -0,0 +1,31 @@
+describe('LibraryEntryView', function() {
+  var view, model;
+
+  beforeEach(function() {
+    model = new SongModel({
+      artist: 'Fakey McFakerson',
+      title: 'Never Gonna Mock You Up',
+      url: 'example/url',
+    });
+    // sinon.spy(SongModel.prototype, 'enqueue'); // Uncomment this when working on the second test
+    sinon.spy(SongModel.prototype, 'play');
+    view = new LibraryEntryView({model: model});
+    view.render();
+  });
+
+  after(function() {
+    SongModel.prototype.enqueue.restore();
+  });
+
+  it('plays clicked songs', function(){
+    view.$el.children().first().click();
+    expect(model.play).to.have.been.called;
+  });
+
+  // Comment out the above spec when implementing the below
+  xit('queues clicked songs', function(){
+    view.$el.children().first().click();
+    expect(model.enqueue).to.have.been.called;
+  });
+
+});
---
+++ test/spec/PlayerViewSpec.js
@@ -0,0 +1,40 @@
+describe('PlayerView', function() {
+  var library, appView;
+
+  beforeEach(function() {
+
+    library = new Songs([
+      {
+        url: "mp3s/08 4 Page Letter.mp3",
+        title: "4 Page Letter",
+        artist: "Aaliyah"
+      },
+      {
+        url: "mp3s/11 We Need A Resolution.mp3",
+        title: "We Need A Resolution",
+        artist: "Aaliyah"
+      }
+    ]);
+    // playerView is created in AppView initialize
+    // access with appView.playerView
+    appView = new AppView({model: new AppModel({library: library})});
+  });
+
+  it('gets its model property set to any song that is played', function(){
+    expect(appView.playerView.model).to.not.equal(library.at(0));
+    library.at(0).play();
+    expect(appView.playerView.model).to.equal(library.at(0));
+  });
+
+  describe('Song transitions', function() {
+    xit('dequeues a song when finished playing & plays the next song', function(){
+      library.at(0).play();
+      var originalSong = appView.playerView.model;
+      appView.model.get('songQueue').add(library.at(1));
+      // Simulate a song end event being triggered
+      $(appView.playerView.el).trigger('ended');
+      expect(appView.playerView.model).to.not.equal(originalSong);
+    });
+  });
+
+});
